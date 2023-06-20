51403b5b3a8bea591a3abc9f025a687a1215bdf4
rktccn <wshhh250@gmail.com>
2023-06-20 20:12:25 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-header class="header" height="60px"><h1 class="title">数据分析</h1></el-header>
+      <el-container>
+        <el-aside width="220px" class="aside">
+          <a-side/>
+        </el-aside>
+        <el-main class="main">Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script setup>
+
+
+import ASide from "@/components/ASide.vue";
+</script>
+
+<style lang="scss" scoped>
+.common-layout {
+  height: 100vh;
+
+  .el-container {
+    height: 100%;
+  }
+
+  .header {
+    background-color: #fff;
+  }
+
+  .title {
+    color: #7FC6D8;
+    font-size: 24px;
+    font-weight: 700;
+    margin: 0;
+    padding: 8px 12px;
+    letter-spacing: 2px;
+  }
+
+  .aside {
+    padding: 18px 12px;
+  }
+
+}
+
+</style>
---
+++ src/components/ASide.vue
@@ -0,0 +1,52 @@
+<template>
+  <el-menu
+      default-active="1"
+      class="aside"
+      @open="handleOpen"
+      @close="handleClose"
+  >
+    <el-menu-item index="1">
+      <span>关联规则</span>
+    </el-menu-item>
+
+    <el-menu-item index="2">
+      <span>聚类分析</span>
+    </el-menu-item>
+
+    <el-menu-item index="3">
+      <span>分类分析</span>
+    </el-menu-item>
+
+    <el-menu-item index="4">
+      <span>回归分析</span>
+    </el-menu-item>
+  </el-menu>
+
+</template>
+
+<script setup>
+const handleOpen = (key, keyPath) => {
+  console.log(key, keyPath)
+}
+
+const handleClose = (key, keyPath) => {
+  console.log(key, keyPath)
+}
+
+</script>
+
+<style lang="scss" scoped>
+.aside {
+  height: 100%;
+  color: #fff;
+  border-radius: 25px;
+}
+
+.el-menu {
+  border-right: none;
+}
+
+.el-menu-item{
+  border-radius: 15px;
+}
+</style>
