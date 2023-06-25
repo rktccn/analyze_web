d1b1c0a70b917c7257b86711a1df73845821dcdf
rktccn <wshhh250@gmail.com>
2023-06-25 16:01:00 +0800

    update

--- src/components/loadData.vue
+++ src/components/loadData.vue
@@ -81,27 +81,6 @@ const generateColumns = (length = 10, prefix = 'column-', props) =>
       title: `Column ${columnIndex}`,
       width: 150
     }))
-
-
-// const generateData = (
-//     columns,
-//     length = 200,
-//     prefix = 'row-'
-// ) =>
-//     Array.from({length}).map((_, rowIndex) => {
-//       return columns.reduce(
-//           (rowData, column, columnIndex) => {
-//             rowData[column.dataKey] = `Row ${rowIndex} - Col ${columnIndex}`
-//             return rowData
-//           }, {
-//             id: `${prefix}${rowIndex}`
-//             // parentId: null
-//           }
-//       )
-//     })
-//
-// const columns = generateColumns(10)
-// const data = generateData(columns, 1000)
 </script>
 
 <style lang='scss' scoped>
