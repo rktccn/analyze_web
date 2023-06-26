576e5312b91da7058b3221bc7d788128af4d9ffa
rktccn <wshhh250@gmail.com>
2023-06-26 13:58:24 +0800

    Update index.js

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,15 +1,59 @@
-// 获取样本
-// {
-//  "code": 0,
-//  "data": [{
-//      key:value
-//      id
-//   }],
-//  }
+import http from '../utils/http.js'
+
+
+// 获取数据
+const getData = (type) => {
+    return http({
+        url: '',
+        method: 'get',
+        data: {
+            type
+        }
+    })
+}
 
 // 获取热力图数据
-// {
-//  "code": 0,
-//  "data": [[横坐标，纵坐标，权重]],
-//  }
+const getHeatMapData = () => {
+    return http({
+        url: '',
+        method: 'get',
+    })
+}
 
+// 获取散点图数据
+const getScatterData = () => {
+    return http({
+        url: '',
+        method: 'get',
+    })
+}
+
+// 获取聚类数据
+const getClusterData = (type) => {
+    return http({
+        url: '',
+        method: 'get',
+        data: {
+            type
+        }
+    })
+}
+
+// 获取分类树
+const getTreeData = () => {
+    return http({
+        url: '',
+        method: 'get',
+    })
+}
+
+// 获取回归
+const getRegressionData = (type) => {
+    return http({
+        url: '',
+        method: 'get',
+        data: {
+            type
+        }
+    })
+}
